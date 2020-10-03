6696ef2e146673dbdcbd06f5a6a78a13d1e1832e
MarkYav <39382424+MarkYav@users.noreply.github.com>
2020-10-03 16:04:25 +0300

    Update main.cpp
    I have added dividion

--- Laba_1/main.cpp
+++ Laba_1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
 
@@ -22,6 +23,7 @@ struct LongNumber
     int lengTop;
     int dot = 0;
     int sing = 1;
+    int presice = 3;
     vector < int > arrayOfTopNumbers;
 
     // functions of LongNumber class
@@ -31,8 +33,8 @@ struct LongNumber
             if (str[i] != '.')
                 arrayOfNumbers.push_back(str[i] - 48);
             else {
-                dot = i;
                 lengTop--;
+                dot = str.size() - 1 - i;
             }
         }
     }
@@ -50,6 +52,7 @@ struct LongNumber
                 break;
         }
         SetPartOfNumber(lengTop, arrayOfTopNumbers, str);
+        ZipNumber();
     }
 
     bool IsHasNumbers(){
@@ -68,8 +71,10 @@ struct LongNumber
         if (sing < 0)
             cout << "-";
         for (int i = lengTop - 1; i >= 0; i--){
+            if (lengTop == dot && i == lengTop - 1)
+                cout << "0.";
             cout << arrayOfTopNumbers[i];
-            if (i == dot)
+            if (i == dot && dot > 0)
                 cout << '.';
         }
     }
@@ -83,9 +88,9 @@ struct LongNumber
     }
 
     bool CompareNumber(const LongNumber &N1, const LongNumber &N2){ //return 1 if N1 >= N2, return 0 if N1 < N2
-        if (N1.lengTop > N2.lengTop)
+        if (N1.lengTop - N1.dot > N2.lengTop - N2.dot)
             return true;
-        else if (N1.lengTop < N2.lengTop)
+        else if (N1.lengTop - N1.dot < N2.lengTop - N2.dot)
             return false;
         else {
             for (int i = N1.lengTop - 1; i >= 0; i--){
@@ -98,7 +103,7 @@ struct LongNumber
         return true;
     }
 
-    bool FinfBiggerNumber(const LongNumber &other){     //return 1 if this >= other, return 0 if this < other
+    bool FindBiggerNumber(const LongNumber &other){     //return 1 if this >= other, return 0 if this < other
         /*if (sing > other.sing)
             return true;
         else if (sing < other.sing)
@@ -118,9 +123,55 @@ struct LongNumber
         }
     }
 
+    void MatchDots(LongNumber &B, LongNumber &S){
+        if (B.dot < S.dot)
+            for (int i = 0; i < S.dot - B.dot; i++){
+                B.arrayOfTopNumbers.emplace(B.arrayOfTopNumbers.begin(), 0);
+                B.lengTop++;
+                B.dot++;
+            }
+        else if (B.dot > S.dot)
+            MatchDots(S, B);
+    }
+
+    void ZipForvardPartOfNumber() {
+        if (arrayOfTopNumbers[lengTop - 1] == 0 && lengTop - 1 - dot > 0) {
+            arrayOfTopNumbers.erase(arrayOfTopNumbers.end() - 1);
+            lengTop--;
+            ZipLastPartOfNumber();
+        }
+
+        /*if (arrayOfTopNumbers[0] == 0) {
+            arrayOfTopNumbers.erase(arrayOfTopNumbers.begin());
+            lengTop--;
+            ZipForvardPartOfNumber();
+        }*/
+    }
+
+    void ZipLastPartOfNumber() {
+        if (arrayOfTopNumbers[0] == 0 && dot > 0) {
+            arrayOfTopNumbers.erase(arrayOfTopNumbers.begin());
+            lengTop--;
+            dot--;
+            ZipForvardPartOfNumber();
+        }
+
+        /*if (dot > 0 && arrayOfTopNumbers[lengTop - 1] == 0) {
+            arrayOfTopNumbers.erase(arrayOfTopNumbers.end() - 1);
+            lengTop--;
+            ZipLastPartOfNumber();
+        }*/
+    }
+
+    void ZipNumber() {
+        ZipForvardPartOfNumber();
+        ZipLastPartOfNumber();
+    }
+
     LongNumber SumOfTwo(const LongNumber &B, const LongNumber &S){
         LongNumber Bigger = B;
         LongNumber Smaller = S;
+        MatchDots(Bigger, Smaller);
         int maxSize = FindMaxLeng(Bigger.lengTop, Smaller.lengTop);
         for (int i = 0; i < maxSize; i++){
             if ( i < Bigger.lengTop && i < Smaller.lengTop){
@@ -160,22 +211,36 @@ struct LongNumber
         }
         lengTop = Bigger.arrayOfTopNumbers.size();
 
+        Bigger.ZipNumber();
         return Bigger;
     }
 
     LongNumber operator+ (const LongNumber &other) {
-        if (FinfBiggerNumber(other)){
+        if (FindBiggerNumber(other)){
             return SumOfTwo(*this, other);
         } else {
             return SumOfTwo(other, *this);
         }
     }
 
-    /*ongNumber operator* (const int &other) {
-        for (int i = lengTop - 1; i >= 0; i--)
+    //TODO почему-то не работает; сделал костыль через следующюю ф-ю
+    /*LongNumber operator* (const int &other) {
+        for (int i = 0; i < other; i++){
+            arrayOfTopNumbers.emplace(arrayOfTopNumbers.begin(), 0);
+            lengTop++;
+        }
+
+        /*for (int i = lengTop - 1; i >= 0; i--)
             arrayOfTopNumbers[i] *= other;
-        return
-    }*/
+        return*/
+    /*}*/
+
+    void MunliplyOnNumber(LongNumber &longN, const int &number){
+        for (int i = 0; i < number; i++){
+            longN.arrayOfTopNumbers.emplace(longN.arrayOfTopNumbers.begin(), 0);
+            longN.lengTop++;
+        }
+    }
 
     LongNumber operator* (const LongNumber &other) {
         LongNumber subArray[other.lengTop];
@@ -203,28 +268,68 @@ struct LongNumber
         }
         result.lengTop = result.arrayOfTopNumbers.size();
 
+        result.ZipNumber();
         return result;
     }
 
     LongNumber operator- (const LongNumber &other) {
         LongNumber copyOfOther = other;
         copyOfOther.ChangeSing();
-        if (FinfBiggerNumber(other)){
+        if (FindBiggerNumber(other)){
             return SumOfTwo(*this, copyOfOther);
         } else {
             return SumOfTwo(copyOfOther, *this);
         }
     }
 
+    LongNumber operator/ (const LongNumber &o) {
+        LongNumber result;
+        LongNumber other = o;
+        result.lengTop = 0;
+        result.sing = sing * other.sing;
+        result.dot = 3;
+
+        MatchDots(*this, other);
+        dot = 0;
+        other.dot = 0;
+        /*for (int i = 0; i < presice; i++){
+            arrayOfTopNumbers.emplace(arrayOfTopNumbers.begin(), 0);
+            lengTop++;
+        }*/
+        //TODO не работает. Сделал костыль
+        //this = this * presice;
+        MunliplyOnNumber(*this, presice);
+
+        for (int i = lengTop - other.lengTop; i >= 0; i--){
+            int num = 0;
+            LongNumber copyOfOther = other;
+            MunliplyOnNumber(copyOfOther, i);
+            while (CompareNumber(*this, copyOfOther) /*|| (lengTop != 1 && arrayOfTopNumbers[0] != 0)*/){
+                //TODO не работает вычитание. использую костыль
+                *this = *this - copyOfOther;
+                num++;
+            }
+            result.arrayOfTopNumbers.push_back(num);
+            result.lengTop++;
+        }
+        reverse(result.arrayOfTopNumbers.begin(), result.arrayOfTopNumbers.end());
+
+        result.ZipNumber();
+        return result;
+    }
+
     void operator= (const LongNumber &other) {
         this->lengTop = other.lengTop;
         this->sing = other.sing;
+        this->dot = other.dot;
 
         this->arrayOfTopNumbers.clear();
 
         for (int i = 0; i < this->lengTop; i++){
             arrayOfTopNumbers.push_back(other.arrayOfTopNumbers[i]);
         }
+
+        ZipNumber();
     }
 
     // constructors
@@ -233,6 +338,7 @@ struct LongNumber
     LongNumber(string str) {
         SetNumber(str);
     }
+
 };
 
 struct LongFruction
@@ -252,6 +358,9 @@ int main() {
     cin >> str1 >> str2;
     firstNumber.SetNumber(str1);
     secondNumber.SetNumber(str2);
-    firstNumber = firstNumber * secondNumber;
+    //firstNumber = firstNumber + secondNumber;
+    //firstNumber = firstNumber - secondNumber;
+    //firstNumber = firstNumber * secondNumber;
+    firstNumber = firstNumber / secondNumber;
     firstNumber.GetNumber();
 }
