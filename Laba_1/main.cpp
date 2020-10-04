c9c3cbf8d1b808d170bd172df475a96b9c93ebb7
MarkYav <39382424+MarkYav@users.noreply.github.com>
2020-10-04 19:40:33 +0300

    Update main.cpp

--- Laba_1/main.cpp
+++ Laba_1/main.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <sstream>
+#include <math.h>
 
 using namespace std;
 
@@ -17,13 +19,13 @@ struct LongFunction;*/
     }
 }*/
 
-//TODO structures
 struct LongNumber
 {
     int lengTop;
     int dot = 0;
     int sing = 1;
     int presice = 3;
+    int base = 10;
     vector < int > arrayOfTopNumbers;
 
     // functions of LongNumber class
@@ -65,7 +67,7 @@ struct LongNumber
 
     void GetNumber(){
         if (!IsHasNumbers()) {
-            cout << 0 << endl;
+            cout << "0";
             return;
         }
         if (sing < 0)
@@ -73,10 +75,37 @@ struct LongNumber
         for (int i = lengTop - 1; i >= 0; i--){
             if (lengTop == dot && i == lengTop - 1)
                 cout << "0.";
-            cout << arrayOfTopNumbers[i];
+            if (arrayOfTopNumbers[i] < 10)
+                cout << arrayOfTopNumbers[i];
+            else
+                switch (arrayOfTopNumbers[i]) {
+                    case 10: cout << "A"; break;
+                    case 11: cout << "B"; break;
+                    case 12: cout << "C"; break;
+                    case 13: cout << "D"; break;
+                    case 14: cout << "E"; break;
+                    case 15: cout << "F"; break;
+                }
             if (i == dot && dot > 0)
                 cout << '.';
         }
+    }
+
+    string GetStringNumber() {
+        if (!IsHasNumbers()) {
+            return "0";
+        }
+        string str;
+        if (sing < 0)
+            str = str + '-';
+        for (int i = lengTop - 1; i >= 0; i--){
+            if (lengTop == dot && i == lengTop - 1)
+                str = str + '0' + '.';
+            cout << arrayOfTopNumbers[i];
+            if (i == dot && dot > 0)
+                str = str + '.';
+        }
+        return str;
     }
 
     int FindMaxLeng(int leng1, int leng2){
@@ -124,12 +153,14 @@ struct LongNumber
     }
 
     void MatchDots(LongNumber &B, LongNumber &S){
-        if (B.dot < S.dot)
-            for (int i = 0; i < S.dot - B.dot; i++){
+        if (B.dot < S.dot) {
+            int num = S.dot - B.dot;
+            for (int i = 0; i < num; i++) {
                 B.arrayOfTopNumbers.emplace(B.arrayOfTopNumbers.begin(), 0);
                 B.lengTop++;
                 B.dot++;
             }
+        }
         else if (B.dot > S.dot)
             MatchDots(S, B);
     }
@@ -166,6 +197,47 @@ struct LongNumber
     void ZipNumber() {
         ZipForvardPartOfNumber();
         ZipLastPartOfNumber();
+    }
+
+    LongNumber ConvertToOtherBase(int newBase) {
+        LongNumber result;
+        LongNumber numberBase(to_string(newBase));
+
+        result.lengTop= 0;
+        result.sing = sing;
+        result.base = newBase;
+        while (CompareNumber(*this, numberBase)) {
+            LongNumber newThis;
+            newThis = *this;
+            newThis = newThis % numberBase;
+            //TODO ко-ко-костылЪ
+            //string str = newThis.GetStringNumber();
+            //stringstream geek(str);
+            //int i = stoi(newThis.GetStringNumber());
+            int sum = 0;
+            for (int i = 0; i < newThis.lengTop; i++){
+                sum += newThis.arrayOfTopNumbers[i] * pow(10, i);
+            }
+            //geek >> i;
+            result.arrayOfTopNumbers.push_back(sum);
+
+            *this = *this ^ numberBase;
+        }
+        /*stringstream geek(this->GetStringNumber());
+        int i = 0;
+        geek >> i;
+        result.arrayOfTopNumbers.push_back(i);*/
+        int sum = 0;
+        for (int i = 0; i < lengTop; i++){
+            sum += arrayOfTopNumbers[i] * pow(10, i);
+        }
+        //geek >> i;
+        result.arrayOfTopNumbers.push_back(sum);
+        result.lengTop = result.arrayOfTopNumbers.size();
+
+        //TODO костыль: не работает присвоение; сделал через return
+        //*this = result;
+        return result;
     }
 
     LongNumber SumOfTwo(const LongNumber &B, const LongNumber &S){
@@ -254,15 +326,26 @@ struct LongNumber
                 subArray[i].arrayOfTopNumbers.push_back(0);
                 subArray[i].lengTop++;
             }
+            int inherter = 0;
             for (int k = 0; k < lengTop; k++){
                 if (arrayOfTopNumbers[k]*other.arrayOfTopNumbers[i] < 10) {
-                    subArray[i].arrayOfTopNumbers.push_back(arrayOfTopNumbers[k] * other.arrayOfTopNumbers[i]);
+                    if (inherter > 0){
+                        subArray[i].arrayOfTopNumbers.push_back(arrayOfTopNumbers[k] * other.arrayOfTopNumbers[i] + inherter);
+                        inherter = 0;
+                        }
+                    else
+                        subArray[i].arrayOfTopNumbers.push_back(arrayOfTopNumbers[k] * other.arrayOfTopNumbers[i]);
                     subArray[i].lengTop++;
                 } else {
                     subArray[i].arrayOfTopNumbers.push_back(arrayOfTopNumbers[k] * other.arrayOfTopNumbers[i] % 10);
-                    subArray[i].arrayOfTopNumbers.push_back(arrayOfTopNumbers[k] * other.arrayOfTopNumbers[i] / 10);
-                    subArray[i].lengTop += 2;
+                    inherter = (arrayOfTopNumbers[k] * other.arrayOfTopNumbers[i]) / 10;
+                    subArray[i].lengTop ++;
                 }
+            }
+            if (inherter > 0) {
+                subArray[i].arrayOfTopNumbers.push_back(inherter);
+                subArray[i].lengTop += inherter;
+                inherter = 0;
             }
             result = result + subArray[i];
         }
@@ -318,10 +401,53 @@ struct LongNumber
         return result;
     }
 
+    LongNumber operator^ (const LongNumber &o) {
+        LongNumber result;
+        LongNumber other = o;
+        result.lengTop = 0;
+        result.sing = sing * other.sing;
+        result.dot = 0;
+
+        MatchDots(*this, other);
+        dot = 0;
+        other.dot = 0;
+
+        for (int i = lengTop - other.lengTop; i >= 0; i--){
+            int num = 0;
+            LongNumber copyOfOther = other;
+            MunliplyOnNumber(copyOfOther, i);
+            while (CompareNumber(*this, copyOfOther) /*|| (lengTop != 1 && arrayOfTopNumbers[0] != 0)*/){
+                //TODO не работает вычитание. использую костыль
+                *this = *this - copyOfOther;
+                num++;
+            }
+            result.arrayOfTopNumbers.push_back(num);
+            result.lengTop++;
+        }
+        reverse(result.arrayOfTopNumbers.begin(), result.arrayOfTopNumbers.end());
+
+        result.ZipNumber();
+        return result;
+    }
+
+    LongNumber operator% (const LongNumber &other) {
+        LongNumber th1, th2;
+        th1 = *this;
+        th2 = *this;
+        LongNumber R1 = th1;
+        LongNumber R2 = (th2 ^ other);
+        R2 = R2 * other;
+        R1 = R1 - R2;
+        //R1 = R1 * other;
+        return R1;
+    }
+
     void operator= (const LongNumber &other) {
         this->lengTop = other.lengTop;
         this->sing = other.sing;
         this->dot = other.dot;
+        this->presice = other.presice;
+        this->base = other.base;
 
         this->arrayOfTopNumbers.clear();
 
@@ -354,13 +480,20 @@ struct LongFruction
 
 int main() {
     LongNumber firstNumber, secondNumber;
-    string str1, str2;
-    cin >> str1 >> str2;
+    string str1;
+    string str2;
+    int base;
+    cin >> str1;
+    cin >> str2;
+    //cin >> base;
     firstNumber.SetNumber(str1);
     secondNumber.SetNumber(str2);
-    //firstNumber = firstNumber + secondNumber;
+    firstNumber = firstNumber + secondNumber;
     //firstNumber = firstNumber - secondNumber;
     //firstNumber = firstNumber * secondNumber;
-    firstNumber = firstNumber / secondNumber;
+    //firstNumber = firstNumber / secondNumber;
+    //firstNumber = firstNumber ^ secondNumber;
+    //firstNumber = firstNumber % secondNumber;
+    //firstNumber = firstNumber.ConvertToOtherBase(base);
     firstNumber.GetNumber();
 }
